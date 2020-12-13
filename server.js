d2a3d0581288d68dc178179821b5b30e99082493
MrzvUz <mrzv.uz@gmail.com>
2020-12-13 10:54:35 -0600

    added mongodb atlas database

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 8080; // Step 1
 
 const routes = require('./routes/api');
 
+mongodb + srv://DevOps:HelloSmart123@cluster0.7mg1o.mongodb.net/Test?retryWrites=true&w=majority
+
 // Step 2
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
     useNewUrlParser: true,
